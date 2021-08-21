2a2c5009902cb4850465b7d961d0ece91540e600
Sanooj A J <sanooj.aj@gmail.com>
2021-08-21 17:08:52 +0000

    corrected helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,9 @@
 public class HelloWorld {
-<<<<<<< HEAD
   public static void main(String[] args) {
     System.out.println("Hello, world!");
   }
-=======
  public static void main(String[] args) {
   System.out.println("Hello, World!");
  }
->>>>>>> 82519edd8b6b1b9c833d996384dd2f602d2b8775
 }
 
