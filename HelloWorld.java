1696af1251f5f907397b7ca9e1d49afe4c0c580d
sanooj A J <SANOOJ@TEST.COM>
2021-10-08 20:39:07 +0000

    MODIFIED HELLOWORLD.JAVA

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world, THIS IS SANOOJ LEARNING JENKINS AND JAVA!");
   }
  public static void main(String[] args) {
-  System.out.println("Hello, World!");
+  System.out.println("Hello, World, THIS IS SANOOJ LEARNING JENKINS AND JAVA!");
  }
 }
 
